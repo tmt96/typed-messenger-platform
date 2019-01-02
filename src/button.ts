0bfac7ff5bafc4a01b6fa121b7cc1d9f6f8a3c33
Tuan Tran <tuanmt@fb.com>
2019-01-02 14:43:53 -0800

    buttons

--- src/button.ts
+++ src/button.ts
@@ -1,3 +1,5 @@
+import { Message } from "./send-api";
+
 /**
  * @module typed-messenger-platform
  */
@@ -12,19 +14,50 @@ export type Button =
   | ShareButton
   | UrlButton;
 
-export interface BuyButton {}
+export interface BuyButton {
+  type: "payment";
+  title: "buy";
+  payload: string;
+  payment_summary: PaymentSummary;
+}
 
-export interface CallButton {}
+export interface CallButton {
+  type: "phone_number";
+  title: string;
+  payload: string;
+}
 
-export interface GameplayButton {}
+export interface GameplayButton {
+  type: "game_play";
+  title: string;
+  payload?: string;
+  game_metadat?: GameMetadata;
+}
 
-export interface LoginButton {}
+export interface LoginButton {
+  type: "account_link";
+  url: string;
+}
 
-export interface LogoutButton {}
+export interface LogoutButton {
+  type: "account_unlink";
+}
 
-export interface PostbackButton {}
+export interface PostbackButton {
+  type: "postback";
+  title: string;
+  payload: string;
+}
 
-export interface ShareButton {}
+export interface ShareButton {
+  type: "element_share";
+  share_contents: {
+    attachments: {
+      type: "template";
+      payload: Message;
+    };
+  };
+}
 
 export interface UrlButton {
   type: "web_url";
@@ -34,4 +67,29 @@ export interface UrlButton {
   messenger_extensions?: boolean;
   fallback_url?: string;
   webview_share_button?: string;
+}
+
+export interface PaymentSummary {
+  currency: string;
+  is_test_payment?: boolean;
+  payment_type: "FIXED_AMOUNT" | "FLEXIBLE_AMOUNT";
+  merchant_type: string;
+  requested_user_info: RequestedUserInfo[];
+  price_list: PriceList[];
+}
+
+export type RequestedUserInfo =
+  | "shipping_address"
+  | "contact_name"
+  | "contact_phone"
+  | "contact_email";
+
+export interface PriceList {
+  label: string;
+  amount: string;
+}
+
+export interface GameMetadata {
+  player_id?: string;
+  context_id?: string;
 }
